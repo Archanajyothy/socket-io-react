1e682ba9ad717344579c76d0a34a6e53b53fe57e
archanajyothy <archanajyothy@gmail.com>
2023-05-25 14:37:51 +0530

    socket.io

--- server/index.js
+++ server/index.js
@@ -10,11 +10,20 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
     cors:{
-        origin:"http://lovalhost:3000",
+        origin:"http://localhost:3000",
         methods:["GET","POST"],
-    }
+    },
 });
+
+io.on("connection",(socket)=>{
+    console.log(`user connected: ${socket.id}`);
+
+    socket.on("send_message",(data)=>{
+        socket.broadcast.emit("receive_message",data)
+    })
+})
 
 server.listen(3001,()=>{
     console.log("SERVER IS RUNNING");
 });
+
